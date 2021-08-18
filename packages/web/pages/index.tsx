d929e012716e0a4c0085fa577dfb6e58fa96c57e
mirzabekov00 <prog.eldar@gmail.com>
2021-08-18 22:32:51 +0300

    feat(web): initialize web package with typescript

---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>UMSF Bot</title>
+      </Head>
+      Home page
+    </div>
+  );
+};
+
+export default Home;
