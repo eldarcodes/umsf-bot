03edf84ab5ba33b8c4c84753daedd09b7aa09911
mirzabekov00 <prog.eldar@gmail.com>
2021-08-18 22:07:17 +0300

    fix(global): commit lint v2

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "scope-enum": [
-      2,
-      "always",
-      [
-        "global",
-        "baklava",
-        "dinner",
-        "kibbeh",
-        "kousa",
-        "pilaf",
-        "shawarma",
-        "kebab",
-        "dolma",
-        "globalkey",
-      ],
-    ],
-  },
 };
