09307fb9ebe322e8bde68fe4b3874a01b3924149
mirzabekov00 <prog.eldar@gmail.com>
2021-08-26 23:51:13 +0300

    feat(global): update auth

--- packages/web/src/pages/auth/[token].tsx
+++ packages/web/src/pages/auth/[token].tsx
@@ -1,33 +1,48 @@
-import { NextPageContext } from "next";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/dist/client/router";
+import { get } from "lodash";
+import { Spin } from "@bot/ui";
 
-interface IAuthPage {
-  data: object;
-}
+const AuthPage: React.FC = () => {
+  const [loading, setLoading] = useState(false);
 
-const AuthPage: React.FC<IAuthPage> = ({ data }) => {
   const router = useRouter();
+  const token = router.query.token;
 
-  return (
-    <>
-      <div>token: {router.query.token}</div>
-      <div>response data: {JSON.stringify(data)}</div>
-    </>
-  );
+  const login = async () => {
+    setLoading(true);
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/login`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          id: token,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const json = await response.json();
+    setLoading(false);
+    const userToken = get(json, "data.token", "");
+
+    localStorage.setItem("token", userToken);
+    router.push("/home");
+  };
+
+  useEffect(() => {
+    if (token) {
+      login();
+    }
+  }, [token]);
+
+  if (loading) {
+    return <Spin />;
+  }
+
+  return null;
 };
-
-export async function getServerSideProps({
-  query,
-}: NextPageContext): Promise<{ props: IAuthPage }> {
-  const { token } = query || {};
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${token}`
-  );
-  const data = await response.json();
-
-  console.log(token, data);
-
-  return { props: { data } };
-}
 
 export default AuthPage;
