5e0dde50cc84fbdda26c609df7a754cdc3345abe
mirzabekov00 <prog.eldar@gmail.com>
2021-08-26 20:42:18 +0300

    feat(web): add language selector

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,14 +1,25 @@
 import { NextPage } from "next";
 import { useTypeSafeTranslation } from "../hooks/useTypeSafeTranslation";
-import { Alert } from "@bot/ui";
+import { Alert, Select, Space } from "@bot/ui";
+import { useTranslation } from "react-i18next";
 
 const Home: NextPage = () => {
+  const { i18n } = useTranslation();
+
   const { t } = useTypeSafeTranslation();
 
   return (
-    <div style={{ padding: 30 }}>
+    <Space style={{ padding: 30 }}>
       <Alert message={t("umsf")} type="info" showIcon />
-    </div>
+      <Select
+        defaultValue="ru"
+        style={{ width: 200 }}
+        onChange={value => i18n.changeLanguage(value)}
+      >
+        <Select.Option value="ru">🇷🇺 Русский</Select.Option>
+        <Select.Option value="uk">🇺🇦 Український</Select.Option>
+      </Select>
+    </Space>
   );
 };
 
