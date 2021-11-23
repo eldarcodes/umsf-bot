df41a29a32e600e1b245815d0dc15cb2eae0fd92
mirzabekov00 <prog.eldar@gmail.com>
2021-11-23 23:31:48 +0200

    fix(api-instance): authorization header

--- packages/web/src/api/instance.ts
+++ packages/web/src/api/instance.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import { isServer } from "../lib/isServer";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 1000,
   headers: {
-    authorization: `Bearer ${localStorage.getItem("token") || ""}`,
+    authorization:
+      (!isServer && `Bearer ${localStorage.getItem("token")}`) || "",
   },
 });
 
