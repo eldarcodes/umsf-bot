51e17d375c2fa1d824fb9f424f4d396176467f9c
mirzabekov00 <prog.eldar@gmail.com>
2021-11-24 23:40:07 +0200

    feat(web): move app routes into separate file

---
+++ packages/web/src/lib/routes.ts
@@ -0,0 +1,16 @@
+import { IRoute } from "../types/routes";
+
+const routes: IRoute[] = [
+  {
+    href: "/",
+    label: "pages.home",
+    icon: "HomeOutlined",
+  },
+  {
+    href: "/dashboard",
+    label: "pages.dashboard",
+    icon: "DashboardOutlined",
+  },
+];
+
+export default routes;
